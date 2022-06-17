0b0a38022d79b967dffced120ec46fdac068ce5e
onaneye <66856751+onaneye@users.noreply.github.com>
2022-06-17 06:45:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(function () {
 
     $(document).ready(function() {
         //Preloader
-        preloaderFadeOutTime = 1000;
+        preloaderFadeOutTime = 500;
         function hidePreloader() {
         var preloader = $('.spinner-wrapper');
         preloader.fadeOut(preloaderFadeOutTime);
